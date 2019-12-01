0c8ae4ae98fc9a799363da443283f9994da4f8a0
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-01 20:49:30 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  attr_reader :name, :backed_projects, :title
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+  end
+
+
+  def name
+    @name
+  end
+
+end
