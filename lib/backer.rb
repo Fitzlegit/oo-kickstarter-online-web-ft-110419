f6376181dd30a67972cc777c0268e8f1bba9d96e
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-01 21:03:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,8 +10,8 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.backers << project
-    binding.pry
+    Project.backers << project
+    #binding.pry
   end
 
 
